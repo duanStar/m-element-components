33b6ed8de81fd167bb1fa130a19f47e3bd10491e
Hongfei Duan <1715106673@qq.com>
2023-01-16 15:27:23 +0800

    feat(*): fix

--- dist/assets/index.fb1176c6.js
+++
@@ -1,1 +0,0 @@
-import{d as t,c as a,a as s,o as l,b as r}from"./index.4fdc9502.js";const h=t({__name:"index",setup(_){const o=e=>{console.log(e)},n=e=>{console.log(e)};return(e,i)=>{const c=r("m-choose-city");return l(),a("div",null,[s(c,{onChangeProvince:n,onChangeCity:o})])}}});export{h as default};
